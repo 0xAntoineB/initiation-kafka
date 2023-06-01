8c1b6557ddfe3c870d0a847344bbc3114d0046c3
root <root@PA-ubuntu.0ufeuwhyxzmehg4n222nwjddmc.parx.internal.cloudapp.net>
2023-06-01 13:10:22 +0000

    First version of my test about kafka, send 3 simples messages

---
+++ python/consumer.py
@@ -0,0 +1,18 @@
+from kafka import KafkaConsumer
+
+bootstrap_servers = '172.20.0.3:29092'
+topic = 'my_topic'
+
+consumer = KafkaConsumer(
+    topic,
+    bootstrap_servers=bootstrap_servers,
+    auto_offset_reset='earliest',
+    enable_auto_commit=True,
+    group_id='my-group'
+)
+
+for message in consumer:
+    print(f"Message reçu: {message.value.decode('utf-8')}")
+
+consumer.close()
+
---
+++ python/producer.py
@@ -0,0 +1,24 @@
+from kafka import KafkaProducer
+
+bootstrap_servers = ':29092'
+topic = 'my_topic'
+
+producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+
+messages = ['Message 1', 'Message 2', 'Message 3']
+
+for message in messages:
+
+    try:
+
+        producer.send(topic, value=message.encode('utf-8'))
+        print(f"Message envoyé : {message}")
+
+    except Exception as e:
+
+        print(f"Erreur lors de l'envoi du message : {message}")
+        print(str(e))
+
+producer.flush()
+producer.close()
+
