bc79d2e45d841886787ba6f55cc236c9bfcaeaeb
Antoine <80824417+0xToonsy@users.noreply.github.com>
2023-06-02 13:45:00 +0200

    Update producersqlcsv.py

--- python/producersqlcsv.py
+++ python/producersqlcsv.py
@@ -5,8 +5,6 @@ def kafka_connect_serveur() -> None:
     """"""
 
     kafka_bootstrap_servers = ':29092'
-    kafka_topic = 'mytopic
-
     producer = KafkaProducer(bootstrap_servers=kafka_bootstrap_servers)
 
     return producer
@@ -14,6 +12,7 @@ def kafka_connect_serveur() -> None:
 def kafka_send_data() -> list:
 
     producer = kafka_connect_serveur()
+    kafka_topic = 'mytopic'
     
     csvfile = ''
 
@@ -26,9 +25,9 @@ def kafka_send_data() -> list:
             prenom = row[1]
             numero = row[2]
 
-            csv_row = ','.join(row)
+            row_ = ','.join(row)
 
-            producer.send(kafka_topic, value=csv_row.encode('utf-8'))
+            producer.send(kafka_topic, value=row_.encode('utf-8'))
 
     producer.close()
 
